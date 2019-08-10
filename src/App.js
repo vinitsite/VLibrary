8da8155436f95601fc2a6ecf5e28a9e04841e2a8
Viint Patil <vinitp836@gmail.com>
2019-08-10 12:19:32 +0530

    created VLibrary Component with basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Spin, Dropdown, Menu, Icon, Layout } from "antd";
+import "./App.css";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+class VLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentLoading: true,
+      listOfCategories: [],
+      booksData: [
+        {
+          name: "Rich Dad Poor Dad",
+          category: "business",
+          author: "Robert Kiyosaki",
+          available: true,
+          rating: 4.5
+        }
+      ]
+    };
+  }
+  componentDidMount = () => {
+    this.setState({
+      componentLoading: false
+    });
+  };
+  menu = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.alipay.com/"
+        >
+          1st menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.taobao.com/"
+        >
+          2nd menu item
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.tmall.com/"
+        >
+          3rd menu item
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+
+  render = () => {
+    const { componentLoading } = this.state;
+    if (componentLoading) {
+      return <Spin tip="Setting Up Things ..."> </Spin>;
+    }
+    const { booksData } = this.state;
+    const { title } = this.props;
+
+    return (
+      <Layout>
+        <Header>
+          <h2 className="title">{title} </h2>
+        </Header>
+        <Layout>
+          <Sider style={styles.sider}>
+            <Dropdown overlay={this.menu}>
+              <a className="ant-dropdown-link" href="#">
+                Hover me <Icon type="down" />
+              </a>
+            </Dropdown>
+          </Sider>
+          <Content style={styles.content}>Content</Content>
+        </Layout>
+        <Footer>
+          Developed By
+          <a href="//vinit.site" rel="noopener noreferrer" target="_blank">
+            {" Vinit Patil "}
+          </a>
+          for Interview an Test at
+          <a href="//sleepycat.in" rel="noopener noreferrer" target="_blank">
+            {" SleepyCat "}
+          </a>
+        </Footer>
+      </Layout>
+    );
+  };
+}
+
+const styles = {
+  sider: {
+    padding: 20
+  },
+  content: {
+    padding: 30
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "cener"
+      }}
+    >
+      <VLibrary title="Vinit's Interview Library" />
     </div>
   );
 }
