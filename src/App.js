fa9cf50d29b5195076d58f04fa186183de96a3db
Viint Patil <vinitp836@gmail.com>
2019-08-10 14:05:24 +0530

    added sample data and removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import {
   Spin,
-  Dropdown,
   Menu,
-  Icon,
   Layout,
   Switch,
   Checkbox,
@@ -186,6 +184,76 @@ const styles = {
 };
 
 function App() {
+  let SampleBooksData = [
+    {
+      id: 1,
+      title: "Rich Dad Poor Dad",
+      category: "business",
+      author: "Robert Kiyosaki",
+      available: true,
+      rating: 4.5
+    },
+    {
+      id: 2,
+      title: "The Great Gatsby",
+      category: "business",
+      author: "F. Scott Fitzgerald",
+      available: false,
+      rating: 4.8
+    },
+    {
+      id: 3,
+      title: "Some Cooking Book",
+      category: "cooking",
+      author: "Harper Lee",
+      available: true,
+      rating: 3
+    },
+    {
+      id: 4,
+      title: "Some LifeStyle Book",
+      category: "lifestyle",
+      author: "J.K. Rowling",
+      available: true,
+      rating: 4.5
+    },
+    {
+      id: 5,
+      title: "1984",
+      category: "business",
+      author: "George Orwell",
+      available: true,
+      rating: 4.5
+    },
+    {
+      id: 6,
+      title: "The Catcher in the Rye",
+      category: "business",
+      author: "J.D. Salinger",
+      available: true,
+      rating: 4.5
+    },
+    {
+      id: 7,
+      title: "The Hobbit",
+      category: "business",
+      author: "J.R.R. Tolkien",
+      available: true,
+      rating: 4.5
+    },
+    {
+      id: 8,
+      title: "Fahrenheit 451",
+      category: "business",
+      author: "Ray Bradbury",
+      available: true,
+      rating: 4.5
+    }
+  ];
+
+  let SampleCategories = ["business", "lifestyle", "cooking"];
+
+  let SampleTitle = "Vinit's Interview Library";
   return (
     <div
       style={{
@@ -193,7 +261,11 @@ function App() {
         justifyContent: "cener"
       }}
     >
-      <VLibrary title="Vinit's Interview Library" />
+      <VLibrary
+        title={SampleTitle}
+        data={SampleBooksData}
+        categories={SampleCategories}
+      />
     </div>
   );
 }
